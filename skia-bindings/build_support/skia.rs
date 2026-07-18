57832c267ddb58223b62388bac02b0b000c19f92
Tiago Dinis <tiagodinis33@gmail.com>
2026-07-18 21:52:28 +0100

    build: re-export configure_skia from skia module

--- skia-bindings/build_support/skia.rs
+++ skia-bindings/build_support/skia.rs
@@ -2,4 +2,4 @@ mod config;
 pub mod env;
 
 #[allow(unused)]
-pub use config::{build, BuildConfiguration, FinalBuildConfiguration};
+pub use config::{build, configure_skia, BuildConfiguration, FinalBuildConfiguration};
